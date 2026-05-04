c397d6394d6737292e7ad9a17e7ddf73c6592ad9
richmondmason-bit <richmond.mason@ucas-edu.net>
2026-05-04 09:31:37 -0600

    COMMITING EVERYWHERE

--- StarterPlayer/StarterCharacterScripts/CharacterHandler.lua
+++
@@ -1,9 +0,0 @@
--- CharacterHandler.localscript - Handles character spawning with blank rig
-local character = script.Parent
-local humanoid = character:WaitForChild("Humanoid")
-local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
-
--- This script runs when the player spawns
--- The server will morph the character into the selected character model during the round
-
-print("Character spawned: " .. character.Name)
